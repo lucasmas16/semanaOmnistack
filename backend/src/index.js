6dbec230a8495df08e6ddcff92c727524183930b
lucasmas16 <lucasmas16@gmail.com>
2020-01-14 23:45:32 -0300

    Cadastro de model, ajuste de rota e chama no github

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,29 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const routes = require('./routes');
 
 const app = express();
 
+//MongoDB (Não-relacional)
 mongoose.connect('mongodb+srv://omnistack:mGMxNQ7gU$D5@cluster0-il0ty.mongodb.net/week10?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
 
 app.use(express.json());
-
-//Metodos HTTP: GET, POST, PUT, DELETE
-
-//Tipos de parametro
-
-//Query Params: request.query (Filtros, ordenação, paginação)
-//Route Params: request.params (Identificar um recurso na alteração ou remoção)
-//Body: request.body (Dados para criação ou alteração de um registro)
-
-//MongoDB (Não-relacional)
-
-
-app.post('/users', (request, response) => {
-    console.log(request.body);
-    return response.json({nome: 'Larissa'});
-});
+app.use(routes);
 
 app.listen(3333);
